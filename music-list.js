e9fad4c5d13363a174616f89a4548fd977ae33c5
panky0026 <panky0026@gmail.comm>
2024-05-07 12:26:37 +0530

    first commit

---
+++ music-list.js
@@ -0,0 +1,45 @@
+let allMusic = [
+    {
+        name: "Gulabi Sharara",
+        artist: "Inder Arya",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "TWEE CHAYEE",
+        artist: "NARENDRA SINGH NEGI",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Hey Madhu",
+        artist: "Inder Arya",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "SUWA RE",
+        artist: "GAJENDER RANA & KAUSHAL PANDEY",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Jhumki",
+        artist: "Priyanka Meher ",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Baand Chon Pahadi",
+        artist: "Karishma Shah",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Balma ",
+        artist: "Meena Rana & Sanjay Kumola",
+        img: "music-7",
+        src: "music-7"
+    },
+
+];
